e72129bb5f262c632db42f383a60e2841e7b606c
Rex Lo <LORE2@OOCL.COM>
2018-12-04 10:56:38 +0800

    feat: onclick increment

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 //import './App.css';
 
+const prop = { number: 0}
+
 class App extends Component {
+  state = {number: 0}
+
+  Click = () =>{
+    //alert("now you know how to handle onclick event");
+    this.setState({
+      number: this.state.number+1
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p><button onClick={Click}>Hello World!</button>number: 0</p>
+          <p><button onClick={this.Click}>Hello World!</button>number: {this.state.number}</p>
         </header>
       </div>
     );
   }
 }
 
-function Click(){
-  alert("now you know how to handle onclick event");
-}
+
 export default App;
